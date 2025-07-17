54055c5fd81e440d9111b71c741ad5ac04eb9ce3
Muhammad Rifqi <muhammad45rifki@gmail.com>
2025-07-17 11:07:10 +0700

    udpate

---
+++ app.js
@@ -0,0 +1,22 @@
+require('dotenv').config();
+const express = require('express');
+const apps = express();
+const cors = require('cors')
+const bodyParser = require('body-parser')
+const cookieParser = require("cookie-parser");
+const db = require('./db/query');
+
+apps.use(cookieParser());
+apps.use(bodyParser.json())
+apps.use(
+    bodyParser.urlencoded({
+        extended: true,
+    })
+)
+apps.use(cors());
+
+apps.post('/api/user', db.userlist);
+
+apps.get('/api/user/:id', db.userdetail);
+
+apps.listen(4001);
---
+++ db/query.js
@@ -0,0 +1,41 @@
+const { executeQuery } = require('./postgres');
+const fs = require('fs');
+
+const userdetail = async (req, res) => {
+    const users_id = req?.params?.id;
+    const query = await executeQuery("SELECT * FROM users where id = $1", [users_id]);
+    if (query.length > 0) {
+        res.status(200).json({ "data": query });
+    } else {
+        res.status(200).json({ "succuss": "failed" })
+    }
+}
+
+const userlist = async (req, res) => {
+    const nameregex = /^[a-zA-Z]+$/;
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    const names = (nameregex.test(req.body.name));
+    const emails = emailRegex.test(req.body.email)
+
+    if (names == false) {
+        res.status(200).json({ "success": false, "message": "nama hanya alpabet" });
+    }
+    else {
+        if (emails == false) {
+            res.status(200).json({ "success": false, "message": "Cek Email Anda" });
+        } else {
+            const sql = await executeQuery("insert into users(name,birth_date,age_years,age_months,address,email)values($1,$2,$3,$4,$5,$6)",
+                [names, req.body.birth_date, req.body.age_years, req.body.age_months, req.body.address, emails]);
+            if (sql) {
+                res.status(200).json({ "success": true, "message": "Data Suda Masuk Ke Database!" });
+            } else {
+                res.status(200).json({ "success": false, "message": "Data Tidak Bisa Masuk Ke Database!" });
+            }
+        }
+    }
+}
+
+module.exports = {
+    userlist,
+    userdetail,
+}
