bb51a2234fd2e95f756a94f1a67e217db4b1dc5c
Muhammad Rifqi <muhammad45rifki@gmail.com>
2025-07-17 12:19:15 +0700

    udpate

--- db/query.js
+++ db/query.js
@@ -26,11 +26,11 @@ const userlist = async (req, res) => {
             res.status(200).json({ "success": false, "message": "Cek Email Anda" });
         } else {
             const sql = await executeQuery("insert into users(name,birth_date,age_years,age_months,address,email)values($1,$2,$3,$4,$5,$6)",
-                [names, req.body.birth_date, req.body.age_years, req.body.age_months, req.body.address, emails]);
+                [char, req.body.birth_date, req.body.age_years, req.body.age_months, req.body.address, emails]);
             if (sql) {
                 res.status(200).json({ "success": true, "message": "Data Suda Masuk Ke Database!" });
             } else {
-                res.status(200).json({ "success": false, "message": "Data Tidak Bisa Masuk Ke Database!" });
+                res.status(403).json({ "success": false, "message": "Data Tidak Bisa Masuk Ke Database!" });
             }
         }
     }
