71d20b71d3cbe5b2007fcd454f03e5991d440c26
Muhammad Rifqi <muhammad45rifki@gmail.com>
2025-07-17 15:29:15 +0700

    udpate

--- db/query.js
+++ db/query.js
@@ -12,10 +12,11 @@ const userdetail = async (req, res) => {
 }
 
 const userlist = async (req, res) => {
-    const nameregex = new RegExp("^[a-z]+$");
+    const nameregex = new RegExp("^[a-zA-Z\\s]+$");
     const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-    const char = req.body.name.toLowerCase();
+    const char = req.body.name;
     const names = (nameregex.test(char));
+    const charemail = req.body.email.trim().toLowerCase();
     const emails = emailRegex.test(req.body.email)
 
     if (names == false) {
@@ -26,7 +27,7 @@ const userlist = async (req, res) => {
             res.status(403).json({ "success": false, "message": "Cek Email Anda" });
         } else {
             const sql = await executeQuery("insert into users(name,birth_date,age_years,age_months,address,email)values($1,$2,$3,$4,$5,$6)",
-                [char, req.body.birth_date, req.body.age_years, req.body.age_months, req.body.address, emails]);
+                [char, req.body.birth_date, req.body.age_years, req.body.age_months, req.body.address, charemail]);
             if (sql) {
                 res.status(200).json({ "success": true, "message": "Data Suda Masuk Ke Database!" });
             } else {
